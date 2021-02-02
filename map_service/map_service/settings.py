1a94ee7af4f34d865b25737874998142f1b1ee00
berthakim <kte.ent@gmail.com>
2021-02-02 23:29:12 +0900

    edit settings

--- map_service/map_service/settings.py
+++ map_service/map_service/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xjfiuno=4htcr6hon0y3^yh^@o2+-%^t&n!r-m_@o6a%qp(p#m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -87,11 +86,6 @@ WSGI_APPLICATION = 'map_service.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'map_services',
-        'USER': 'bertha',
-        'PASSWORD': 'tbs7878',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',  # Set to empty string for default / localhost
     }
 }
 
